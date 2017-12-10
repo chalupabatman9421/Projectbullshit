c79e214de69be79c3f6d625ddc68daef2311b271
chalupabatman9421 <34409714+chalupabatman9421@users.noreply.github.com>
2017-12-09 23:07:55 -0600

    Update tlb.hpp

--- tlb.hpp
+++ tlb.hpp
@@ -2,8 +2,8 @@
 //  tlb.hpp
 //  COMP3500_Project5_TLB
 //
-//  Created by Justin Hahn.
-//  Copyright © 2016 Justin Hahn. All rights reserved.
+//  Created by Robby Lagen on 12/3/16.
+//  Copyright © 2016 Robby Lagen. All rights reserved.
 //
 
 #ifndef tlb_hpp
@@ -34,19 +34,19 @@ using namespace std;
 typedef unsigned char byte;
 
 /* new data type of pages, frames, and offset */
-typedef unsigned int page_t;
+typedef unsigned int tlbPageEntry_t;
 typedef char value_t;
 typedef unsigned int frame_t;
-typedef unsigned int offset_t;
-typedef unsigned int laddress_t;
-typedef unsigned int paddress_t;
+typedef unsigned int offsetAmount_t;
+typedef unsigned int literalAdd_t;
+typedef unsigned int physicalAddress_t;
 typedef char physical_memory_t[NUM_FRAMES][PAGE_SIZE];
-typedef vector<laddress_t> logicAddressList_t;
-typedef vector<paddress_t> physAddressList_t;
+typedef vector<literalAdd_t> logicAddressList_t;
+typedef vector<physicalAddress_t> physAddressList_t;
 typedef vector<char> valueList_t;
 
 typedef struct {
-    page_t pageNum;
+    tlbPageEntry_t pageNum;
     frame_t frameNum;
     bool valid;
     int age;
@@ -67,20 +67,20 @@ typedef struct {
 //typedef frame physical_memory_t[NUM_FRAMES];
 typedef frame_t pageTable_t[NUM_PAGES];
 
-int logicAdrrLoader(string fileName, vector<laddress_t> * logicAddrList);
-int extractLogicAddr(laddress_t address, page_t * pageNum, offset_t * offset);
+int logicAdrrLoader(string fileName, vector<literalAdd_t> * logicAddrList);
+int extractLogicAddr(literalAdd_t address, tlbPageEntry_t * pageNum, offsetAmount_t * offset);
 int initPageTable(pageTable_t pageTable);
 int TLB_init(tlb *tlb);
 int PhsyMemInit(frame physical_memory[NUM_FRAMES]);
-int searchTLB(page_t * pageNum, bool * isTlbHit, frame_t * frameNum, tlb * tlbSearch);
-int searchPageTable(page_t pageNum, bool * isPageFault, frame_t * frameNum, pageTable_t page_Table);
-int handlePageFault(page_t p_num, frame_t *frame_num, frame physical_memory[NUM_FRAMES], pageTable_t p_table, tlb tlb);
-int load_frame_to_physical_memory(page_t pageNum, const char *backingStoreFileName, frame physical_memory[NUM_FRAMES], frame_t *frameNum);
-int createPhysicalAddress(frame_t f_num, offset_t off, paddress_t *physical_addr);
-int TLB_replacement_FIFO(page_t pageNum, frame_t frameNum, tlb *tlb);
-int TLB_replacement_LRU(page_t pageNum, frame_t frameNum, tlb *tlb);
-int readPhysicalMemory (paddress_t p_addr, frame physical_memory[NUM_FRAMES], value_t *value);
-int update_all_lists(paddress_t physAddress, value_t value, physAddressList_t *physAddressList, valueList_t *valueList);
+int searchTLB(tlbPageEntry_t * pageNum, bool * isTlbHit, frame_t * frameNum, tlb * tlbSearch);
+int searchPageTable(tlbPageEntry_t pageNum, bool * isPageFault, frame_t * frameNum, pageTable_t tlbPageEntry_table);
+int handlePageFault(tlbPageEntry_t p_num, frame_t *frame_num, frame physical_memory[NUM_FRAMES], pageTable_t p_table, tlb tlb);
+int load_frame_to_physical_memory(tlbPageEntry_t pageNum, const char *backingStoreFileName, frame physical_memory[NUM_FRAMES], frame_t *frameNum);
+int createPhysicalAddress(frame_t f_num, offsetAmount_t off, physicalAddress_t *physical_addr);
+int TLB_replacement_FIFO(tlbPageEntry_t pageNum, frame_t frameNum, tlb *tlb);
+int TLB_replacement_LRU(tlbPageEntry_t pageNum, frame_t frameNum, tlb *tlb);
+int readPhysicalMemory (physicalAddress_t p_addr, frame physical_memory[NUM_FRAMES], value_t *value);
+int update_all_lists(physicalAddress_t physAddress, value_t value, physAddressList_t *physAddressList, valueList_t *valueList);
 int output_all_lists(logicAddressList_t logicAddrList, physAddressList_t physAddrList, valueList_t valueList, int count);
 int displayAddresses(bool displayAddress, int count, logicAddressList_t logicAddrList, physAddressList_t physAddrList, valueList_t valueList);
 
